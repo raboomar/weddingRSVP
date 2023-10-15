f63b6fb6f59615fd19ad8e5c3c81a395086c9543
Rami <aboomar.2@osu.edu>
2023-10-15 15:01:27 -0400

    Created db and s3 buckets

---
+++ terraform/dtnamodb.tf
@@ -0,0 +1,18 @@
+resource "aws_dynamodb_table" "rsvp_table" {
+  name           = "rsvp-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+}
