8ef3d71e8f0828bfb83940f49ce7310f905e8319
Rami <aboomar.2@osu.edu>
2023-10-17 15:16:45 -0400

    Created Angluar app

---
+++ RSVP-WebApp/src/app/components/guest-form/guest-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-form',
+  templateUrl: './guest-form.component.html',
+  styleUrls: ['./guest-form.component.css']
+})
+export class GuestFormComponent {
+
+}
