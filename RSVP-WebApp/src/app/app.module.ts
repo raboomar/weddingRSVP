8ae64b552ae0574c9843ed2a236613b323a43286
Rami <aboomar.2@osu.edu>
2023-10-18 18:23:51 -0400

    Added form for guest

--- RSVP-WebApp/src/app/app.module.ts
+++ RSVP-WebApp/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuestFormComponent } from './components/guest-form/guest-form.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {MatIconModule} from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import{MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
@@ -12,7 +19,16 @@ import { GuestFormComponent } from './components/guest-form/guest-form.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RSVP-WebApp/src/app/components/guest-form/guest-form.component.ts
+++ RSVP-WebApp/src/app/components/guest-form/guest-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SpinnerServiceService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-guest-form',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./guest-form.component.css']
 })
 export class GuestFormComponent {
+  loading = this.loader.loading$
+  email = new FormControl('', [Validators.required, Validators.email]);
+  public guestForm: FormGroup =new FormGroup({});
+  public validationMsgs = {
+    'firstName': [{ type: 'name', message: 'Enter a name' }]
+  }
+
+  constructor( private formBuilder: FormBuilder, private loader: SpinnerServiceService) { }
+  ngOnInit() {
+    this.guestForm = this.formBuilder.group({
+      name: this.formBuilder.array([this.createGuestFormGroup()])
+    });
+  }
+
+  public removeOrClearEmail(i: number) {
+    const name = this.guestForm.get('name') as FormArray
+    if (name.length > 1) {
+      name.removeAt(i)
+    } else {
+      name.reset()
+    }
+  }
+
+  public addguestFormGroup() {
+    const name = this.guestForm.get('name') as FormArray
+    name.push(this.createGuestFormGroup())
+  }
+
+  private createGuestFormGroup(): FormGroup {
+    return new FormGroup({
+      'firstName': new FormControl('', Validators.min(1)),
+      'lastName': new FormControl('', Validators.required)
+
+    })
+  }
+
+  getControls() {
+    return (this.guestForm.get('name') as FormArray).controls;
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  addGuest(){
+
+    const guestForm = JSON.stringify({
+      "email": this.email.value,
+      "guest":this.guestForm.value.name
+    })
+    // this.isLoading = true
+    // this.formService.addGuest(guestForm).subscribe(res=>{
+    //   this.isLoading = false
+    //   this.succuss = true
+    // })
+  }
 
 }
+
+
