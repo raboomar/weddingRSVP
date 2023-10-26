60ff4836df07f9d3b4907e4c425f302b3f0b7b53
Rami <aboomar.2@osu.edu>
2023-10-25 21:05:06 -0400

    Fixed names

--- backend/email_html.py
+++ backend/email_html.py
@@ -5,7 +5,7 @@ def html_body ():
     <body>
         <div style="display: flex; justify-content: center; text-align: center">
       <h3>
-        Thank you for RSVPing to Rami's & Dimas's wedding.
+        Thank you for RSVPing to Rami & Dima's wedding.
         <br/>
         Date: Sunday, June 2nd, 2024 5:00 pm
         <br />
