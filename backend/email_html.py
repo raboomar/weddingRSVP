bb6bbc88dc4847f8bc314dc551c9c076e29e8029
Rami <aboomar.2@osu.edu>
2023-10-25 20:47:50 -0400

    Updated email

--- backend/email_html.py
+++ backend/email_html.py
@@ -5,13 +5,14 @@ def html_body ():
     <body>
         <div style="display: flex; justify-content: center; text-align: center">
       <h3>
-        Thank you for RSVPing to Ramis & Dimas wedding.
+        Thank you for RSVPing to Ramis & Dimas's wedding.
         <br/>
-        Date: Sunday June 2nd 2024 5:00pm
+        Date: Sunday, June 2nd, 2024 5:00 pm
         <br />
         Location: Pinnacle Golf Club
         <address>1500 Pinnacle Club Dr, Grove City, OH 43123</address>
         <br />
+        If you have any questions blease reach out to Rami or Dima.
         </h3>
         </div>
     </body>
