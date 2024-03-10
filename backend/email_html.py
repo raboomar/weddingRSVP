213500d4c6f4624ef0b9eb25345f76a0f9d23c25
Rami <aboomar.2@osu.edu>
2024-03-10 17:35:07 -0400

    Updated email template

--- backend/email_html.py
+++ backend/email_html.py
@@ -3,6 +3,7 @@ def html_body ():
     body =  """\
    <html>
     <body>
+      <div >
         <div style="display: flex; justify-content: center; text-align: center">
       <h3>
         Thank you for RSVPing to Rami & Dima's wedding.
@@ -12,8 +13,16 @@ def html_body ():
         Location: Pinnacle Golf Club
         <address>1500 Pinnacle Club Dr, Grove City, OH 43123</address>
         <br />
-        If you have any questions please reach out to Rami or Dima.
+        Your presence at our wedding is the greatest gift of all. However, should you wish to honour us with a gift, envelope gifts are preferred.  
         </h3>
+         </div>
+        <div style="display: flex;">
+        <h3>
+        Best regards, 
+         <br />
+        Rami and Dima
+        </h3>
+    </div>
         </div>
     </body>
     </html>
